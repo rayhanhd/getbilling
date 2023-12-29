9ce48989b3896aa4869a4a2017a9558c46d05c1e
rayhanhd <rehanstory@gmail.com>
2023-12-29 16:18:00 +0800

    Update getdata.bat

--- getdata.bat
+++ getdata.bat
@@ -2,15 +2,17 @@
 setlocal enabledelayedexpansion
 
 set "input_folder=C:\Program Files\hms\smdr\copy"
-set "output_extension=txt"
+set "output_file=output_combined.txt"
 
 if not exist "!input_folder!" (
     echo Input folder "!input_folder!" not found.
     exit /b 1
 )
 
+rem Create or clear the output file
+type nul > "%~dp0\!output_file!"
+
 for %%F in ("!input_folder!\*.txt") do (
-    set "input_file=%%~nF"
     set "full_path=%%~fF"
     
     rem Read the content of the input file
@@ -19,10 +21,10 @@ for %%F in ("!input_folder!\*.txt") do (
         set "content=!content!%%a"
     )
 
-    rem Write the content to the output file
-    set "output_file=!input_file!.!output_extension!"
-    echo !content! > "!input_folder!\!output_file!"
-    echo Content from "!input_file!.txt" has been stored in "!output_file!".
+    rem Append the content to the output file
+    echo !content! >> "%~dp0\!output_file!"
 )
+
+echo Combined content from all .txt files has been stored in "!output_file!".
 
 endlocal
